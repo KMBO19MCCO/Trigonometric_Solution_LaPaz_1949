fc2448ce223160f10a38ce333f46d6657c36b22c
MadBunny0 <uspenskiy.artem@yandex.ru>
2022-11-11 15:15:42 +0300

    add rep

---
+++ main.cpp
@@ -0,0 +1,91 @@
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <complex>
+#include <limits>
+#include "excerpt.h"
+
+typedef float fp_t;
+const double PI = acos(-1.0);
+
+using namespace std;
+
+//A simple formula for solving quadratic equations using function evaluation
+template<typename fp_t>
+void trigonometric(fp_t a, fp_t b, fp_t c, fp_t *x0, fp_t *x1) {
+    fp_t A, B, C, TAU;
+    //Coefficients
+    A = a;
+    B = b;
+    C = c;
+    cout << endl << A << "x^2 + " << B << "x + " << C << endl;
+    fp_t arg1 = 2 * sqrt(abs(A * C)) / B;
+    fp_t arg2 = sqrt(abs(C / A));
+    fp_t tetta_p_0 = 0;
+    fp_t tetta_p_1 = 0;
+    fp_t tetta_n_0 = 0;
+    fp_t tetta_n_1 = 0;
+    fp_t tetta0 = 0;
+    if (C > 0 and abs(arg1) <= 1) {
+        TAU = asin(-arg1);
+        tetta_p_0 = TAU;
+        tetta_p_1 = TAU + PI;
+
+        (0 < abs(tetta_p_0) <= PI/2) ? tetta0 = tetta_p_0 : tetta0 = tetta_p_1;
+
+        *x0 = arg2 * tan(tetta0/2);
+        *x1 = arg2 * 1/tan(tetta0/2);
+    } else if (C <= 0) {
+        TAU = atan(arg1);
+        tetta_n_0 = TAU;
+        tetta_n_1 = TAU + PI/2;
+        (0 < abs(tetta_n_0) <= PI/2) ? tetta0 = tetta_n_0 : tetta0 = tetta_n_1;
+
+        *x0 = arg2 * tan(tetta0/2);
+        *x1 = -arg2 * 1/tan(tetta0/2);
+    } else
+        cout << "Only complex roots ";
+    cout << "Roots: " << x0 << "; " << x1 << endl;
+}
+template<typename fp_t>
+auto testPolynomial(unsigned int roots_count) {
+    fp_t x0, x1, max_absolute_error, max_relative_error;
+    vector<fp_t> roots(roots_count), coefficients(roots_count + 1);
+    generate_polynomial<fp_t>(roots_count, 0, roots_count, 0, 1e-5, -1, 1, roots, coefficients);
+    trigonometric<fp_t>(coefficients[2], coefficients[1], coefficients[0], &x0, &x1);
+    vector<fp_t> roots_computed = {x1, x0};
+    auto result = compare_roots<fp_t>(roots_computed.size(), roots.size(), roots_computed, roots, max_absolute_error, max_relative_error);
+    switch (result) {
+        case PR_2_INFINITE_ROOTS:
+            cout << "INFINITE ROOTS";
+            break;
+        case PR_AT_LEAST_ONE_ROOT_IS_FAKE:
+            cout << "AT LEAST ONE ROOT IS FAKE";
+            break;
+        case PR_AT_LEAST_ONE_ROOT_LOST:
+            cout << "AT LEAST ONE ROOT LOST";
+            break;
+        default:
+            break;
+    }
+    return max_absolute_error;
+}
+
+typedef float fp_t;
+
+int main() {
+    float deviation, max_deviation = 0;
+    for (auto i = 0; i < 10'000; ++i) {
+        deviation = testPolynomial<float>(2);
+        if (deviation != std::numeric_limits<float>::infinity()) {
+            cout << "deviation = " << deviation << endl;
+            if (deviation > max_deviation) {
+                max_deviation = deviation;
+            }
+        }
+        else cout << "\t\tComplex roots!"<<endl;
+    }
+    cout<< endl<<"MAX_deviation = "<< max_deviation<<endl;
+
+}
