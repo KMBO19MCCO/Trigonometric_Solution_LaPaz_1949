ac7292ac253737c31360c19fff035a4903fb3bcc
MadBunny0 <uspenskiy.artem@yandex.ru>
2022-12-12 19:57:28 +0300

    add fma

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ int trigonometric(vector<fp_t> coefficients, vector<fp_t> &roots) {
     b = coefficients[1];
     c = coefficients[0];
 
-    cout << endl << a << "x^2 + " << b << "x + " << c << endl; // ax^2+bx+c
+    //cout << endl << a << "x^2 + " << b << "x + " << c << endl; // ax^2+bx+c
     fp_t arg1 = 2 * sqrt(abs(a * c)) / b; //аргумент при tetta
     fp_t arg2 = sqrt(abs(c / a)); //аргумент при x
     fp_t tetta_p;
@@ -35,45 +35,45 @@ int trigonometric(vector<fp_t> coefficients, vector<fp_t> &roots) {
             tetta_p = asin(-arg1);
             tetta_0 = tetta_p;
 
-            while (!(0 < abs(tetta_0) < PI / 2)) // Диапазон
+            while (!(0 < abs(tetta_0) < PI / static_cast<fp_t>(2.0f))) // Диапазон
             {
-                if (tetta_0 > PI/2) { // Если выходит справа
-                    tetta_0 += (tetta_p - j * PI) / pow((-1), j);
+                if (tetta_0 > PI/2) { // Если выходит справа (меняется чётность j)
+                    tetta_0 += fma(-PI,j,tetta_p) / pow((-1), j); //fma(-PI,j,tetta_p) / pow((-1), j), (tetta_p - j * PI) / pow((-1), j)
                     j++;
-                } else { // Если выходит слева
-                    tetta_0 += (tetta_p - j * PI) / pow((-1), j);
+                } else { // Если выходит слева (меняется чётность j)
+                    tetta_0 += fma(-PI,j,tetta_p) / pow((-1), j); // fma(-PI,j,tetta_p) / pow((-1), j, (tetta_p - j * PI) / pow((-1), j)
                     j--;
                 }
             }
 
-            roots[0] = arg2 * tan(tetta_0 / 2);
-            roots[1] = arg2 / tan(tetta_0 / 2);
+            roots[0] = arg2 * tan(tetta_0 / static_cast<fp_t>(2.0f));
+            roots[1] = arg2 / tan(tetta_0 / static_cast<fp_t>(2.0f));
 
-            cout << "Roots: " << roots[0] << "; " << roots[1] << endl;
+            //cout << "Roots: " << roots[0] << "; " << roots[1] << endl;
             cnt_roots = 2;
         } else if (c < 0 and abs(arg1) <= 1) { // Проверка на вещественные корни и "С" меньше 0
             tetta_n = atan(arg1);
             tetta_0 = tetta_n;
 
-            while (!(0 < abs(tetta_0) < PI / 2))
+            while (!(0 < abs(tetta_0) < PI / static_cast<fp_t>(2.0f)))
             {
                 if (tetta_0 > PI/2) { // Если выходит справа
-                    tetta_0 += (tetta_n - j * PI) / pow((-1), j);
+                    tetta_0 += fma(-PI,j,tetta_n) / pow((-1), j); //fma(-PI,j,tetta_n) / pow((-1), j), (tetta_n - j * PI) / pow((-1), j)
                     j++;
                 } else { // Если выходит слева
-                    tetta_0 += (tetta_n - j * PI) / pow((-1), j);
+                    tetta_0 += fma(-PI,j,tetta_n) / pow((-1), j); //(tetta_n - j * PI) / pow((-1), j)
                     j--;
                 }
             }
 
-            roots[0] = arg2 * tan(tetta_0 / 2);
-            roots[1] = -arg2 / tan(tetta_0 / 2);
+            roots[0] = arg2 * tan(tetta_0 / static_cast<fp_t>(2.0f));
+            roots[1] = -arg2 / tan(tetta_0 / static_cast<fp_t>(2.0f));
 
-            cout << "Roots: " << roots[0] << "; " << roots[1] << endl;
+            //cout << "Roots: " << roots[0] << "; " << roots[1] << endl;
             cnt_roots = 2;
         } else { // Если комплексные корни
             cnt_roots = 0;
-            cout << "Only complex roots " << endl;
+            //cout << "Only complex roots " << endl;
         }
     }
     else{          // b/a = inf - значит уравнение формально не квадратное, находим единственный корень
@@ -95,9 +95,8 @@ pair<fp_t, fp_t> testPolynomial(unsigned int roots_count) {
                               coefficients);
     int cnt_real_roots = trigonometric(coefficients, roots_computed);
     if (cnt_real_roots != 0 && cnt_real_roots != -1) {
-        compare_roots2<fp_t>(roots_computed.size(), roots.size(), roots_computed, roots,
+        compare_roots<fp_t>(roots_computed.size(), roots.size(), roots_computed, roots,
                             max_absolute_error, max_relative_error);
-
 
     } else max_absolute_error = 0, max_relative_error = 0;
     return pair<fp_t, fp_t>(max_absolute_error, max_relative_error);
@@ -109,9 +108,9 @@ typedef float fp_t;
 int main() {
     float max_absolut_deviation = 0;
     float max_relative_deviation = 0;
-    for (auto i = 0; i < 10'000; ++i) {
+    for (auto i = 0; i < 10'000'000; ++i) {
         auto deviation = testPolynomial<float>(2);
-        cout << "ABS_DEVATION = " << deviation.first << endl;
+        //cout << "ABS_DEVATION = " << deviation.first << endl;
 
         if (deviation.first > max_absolut_deviation) {
             max_absolut_deviation = deviation.first;
